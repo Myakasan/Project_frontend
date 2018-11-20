6cc38de619ce4ff6bc5e2d21b4c79a0ee3373cb1
Etienne Mya K <egendron6@icloud.com>
2018-11-20 14:19:54 +0100

    maJolieNavbar
    La nav est jolie du cul

--- script/nav.php
+++ script/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark clearfix shadow-sm">
   <a class="navbar-brand" href="#">
     <img src="./img/image10.png" width="30" height="30" class="d-inline-block align-top" alt="">
     L33T SH0PPING
@@ -10,44 +10,45 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#"><i class="fas fa-home"></i> Accueil <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Homme
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">T-Shirt</a>
+          <a class="dropdown-item" href="#">Hoodies</a>
+          <a class="dropdown-item" href="#">Pantalon</a>
         </div>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Femme
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">T-Shirt</a>
+          <a class="dropdown-item" href="#">Hoodies</a>
+          <a class="dropdown-item" href="#">Pantalon</a>
         </div>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Enfant
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">T-Shirt</a>
+          <a class="dropdown-item" href="#">Hoodies</a>
+          <a class="dropdown-item" href="#">Pantalon</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Connexion</a>
+      <li class="nav-item float-right">
+        <a class="nav-link" href="#"><i class="fas fa-lock"></i> Connexion</a>
       </li>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i> Panier</a>
+    </li>
     </ul>
 
   </div>
