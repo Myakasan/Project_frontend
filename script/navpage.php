6daceba01e4c40d320d0e68860b592027b37470a
Ressort Aloïs <ressort.alois@gmail.com>
2018-11-21 14:38:19 +0100

    fixpourris

--- script/navpage.php
+++ script/navpage.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark clearfix shadow-sm">
   <a class="navbar-brand" href="#">
-    <img src="./img/image10.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    <img src="../img/image10.png" width="30" height="30" class="d-inline-block align-top" alt="">
     L33T SH0PPING
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,7 +10,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="index.html"><i class="fas fa-home"></i> Accueil <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="./index.php"><i class="fas fa-home"></i> Accueil <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
