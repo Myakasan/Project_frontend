ba57aa55b43e5fec9fa6d5b5355c6cba0bed0c57
Ressort Alois <ressort.alois@gmail.com>
2018-11-22 00:38:10 +0100

    Remplissage de la page détail et j'ai sommeil

--- page/details.php
+++ page/details.php
@@ -29,28 +29,90 @@
   </nav>
   <hr>
   <div class="article-details">
-    <div class="container">
+    <div class="container-fluid">
   <div class="row">
     <div class="col">
-      <img class="border border-danger rounded" src="../img/tee1.jpg" alt="">
+      <img class="rounded img-thumbnail" src="../img/tee1.jpg" alt="">
     </div>
     <div class="col">
-      One of three columns
+      <h4>Detail du produit</h4>
+      <h5>T-shirt Homme Lord of The Drinks</h5>
+      <p>Un classique sans compromis : vous allez adorer ce T-shirt simple et classique.
+         Un basique à porter en toutes occasions.</p>
+      <ul>
+        <li>Finitions haut de gamme avec doubles coutures</li>
+        <li>En tailles 3XL et 4XL, seules les couleurs suivantes sont proposées par le fabricant : blanc, noir, bleu marine, rouge et gris chiné</li>
+        <li>Coupe droite</li>
+        <li>Tissu résistant : 185 g/m²</li>
+        <li>Composition : 100 % coton (sauf en gris chiné : 85 % coton, 15 % viscose)</li>
+      </ul>
     </div>
     <div class="col">
-      One of three columns
+      <h3>Choisissez la couleur :</h3>
+      <div class="btn-toolbar" role="toolbar" aria-label="Color Butt">
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-light btn-lg rounded-circle">LG</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-sucess btn-lg rounded-circle">MG</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-secondary btn-lg rounded-circle">DG</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-dark btn-lg rounded-circle">DK</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-danger btn-lg rounded-circle">DR</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-primary btn-lg rounded-circle">MB</button>
+        </div>
+      </div>
+      <h3>Choisissez la taille :</h3>
+      <div class="btn-toolbar" role="toolbar" aria-label="Size Butt">
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-sucess btn-lg">S</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-sucess btn-lg">M</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-sucess btn-lg">L</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-sucess btn-lg">XL</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-sucess btn-lg">XXL</button>
+        </div>
+        <div class="btn-group mr-2 mb-2" role="group" aria-label="Third group">
+          <button type="button" class="btn btn-sucess btn-lg">XXXL</button>
+        </div>
+      </div>
+      <h3>Choisissez la quantité :</h3>
+      <div class="form-group">
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <button type="button" class="btn btn-danger btn-lg btn-block">Ajouter au panier</button>
     </div>
   </div>
-
+    <h1 class="mb-3" style="text-align: center;">Ces articles pourraient vous intérésser !</h1>
   <div class="row">
     <div class="col">
-      One of three columns
+      SUGG
     </div>
     <div class="col">
-      One of three columns
+      SUGG
     </div>
     <div class="col">
-      One of three columns
+      SUGG
     </div>
   </div>
   </div>
