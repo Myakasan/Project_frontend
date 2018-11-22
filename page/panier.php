b389c97e34e6b6235721154287fee260f19782fe
Ressort Alois <ressort.alois@gmail.com>
2018-11-22 23:07:52 +0100

    finalv1

--- page/panier.php
+++ page/panier.php
@@ -23,79 +23,91 @@
   </nav>
 
   <div class="monPanierResponsive">
-    <h1>Shopping Cart</h1>
+    <h1>Votre panier</h1>
 
   <div class="shopping-cart">
 
     <div class="column-labels">
       <label class="product-image">Image</label>
       <label class="product-details">Product</label>
-      <label class="product-price">Price</label>
-      <label class="product-quantity">Quantity</label>
+      <label class="product-price">Prix</label>
+      <label class="product-quantity">Quantité</label>
       <label class="product-removal">Remove</label>
       <label class="product-line-price">Total</label>
     </div>
 
     <div class="product">
       <div class="product-image">
-        <img src="https://s.cdpn.io/3/dingo-dog-bones.jpg">
+        <img src="../img/tee1.jpg">
       </div>
       <div class="product-details">
-        <div class="product-title">Dingo Dog Bones</div>
-        <p class="product-description">The best dog bones of all time. Holy crap. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.</p>
+        <div class="product-title">The Lord of Drinks</div>
+        <p class="product-description">Il n'y a pourtant pas de bar dans les ruines de la moria...</p>
       </div>
-      <div class="product-price">12.99</div>
+      <div class="product-price">22.00</div>
       <div class="product-quantity">
         <input type="number" value="2" min="1">
       </div>
       <div class="product-removal">
         <button class="remove-product">
-          Remove
+          Retirer
+        </button>
+        <button class="update-product">
+          Mettre à jour
+        </button>
+        <button class="view-product">
+          Voir la fiche du produit
         </button>
       </div>
-      <div class="product-line-price">25.98</div>
+      <div class="product-line-price">44.00</div>
     </div>
 
     <div class="product">
       <div class="product-image">
-        <img src="https://s.cdpn.io/3/large-NutroNaturalChoiceAdultLambMealandRiceDryDogFood.png">
+        <img src="../img/tee3.jpg">
       </div>
       <div class="product-details">
-        <div class="product-title">Nutro™ Adult Lamb and Rice Dog Food</div>
-        <p class="product-description">Who doesn't like lamb and rice? We've all hit the halal cart at 3am while quasi-blackout after a night of binge drinking in Manhattan. Now it's your dog's turn!</p>
+        <div class="product-title">Call me Unicorn</div>
+        <p class="product-description">Qui à dis que les chats n'avaient pas le droit de rêver ?!?</p>
       </div>
-      <div class="product-price">45.99</div>
+      <div class="product-price">20.00</div>
       <div class="product-quantity">
         <input type="number" value="1" min="1">
       </div>
       <div class="product-removal">
         <button class="remove-product">
-          Remove
+          Retirer
+        </button>
+        <button class="update-product">
+          Mettre à jour
+        </button>
+        <button class="view-product">
+          Voir la fiche du produit
         </button>
       </div>
-      <div class="product-line-price">45.99</div>
+      <div class="product-line-price">20.00</div>
     </div>
 
     <div class="totals">
       <div class="totals-item">
-        <label>Subtotal</label>
-        <div class="totals-value" id="cart-subtotal">71.97</div>
+        <label>Sous-total</label>
+        <div class="totals-value" id="cart-subtotal">64.00</div>
       </div>
       <div class="totals-item">
-        <label>Tax (5%)</label>
-        <div class="totals-value" id="cart-tax">3.60</div>
+        <label>TVA (20%)</label>
+        <div class="totals-value" id="cart-tax">12.80</div>
       </div>
       <div class="totals-item">
-        <label>Shipping</label>
-        <div class="totals-value" id="cart-shipping">15.00</div>
+        <label>Frais de livraison</label>
+        <div class="totals-value" id="cart-shipping">10.00</div>
       </div>
       <div class="totals-item totals-item-total">
-        <label>Grand Total</label>
-        <div class="totals-value" id="cart-total">90.57</div>
+        <label>Total TTC</label>
+        <div class="totals-value" id="cart-total">96.80</div>
       </div>
     </div>
 
-        <button class="checkout">Checkout</button>
+        <button class="checkout">Valider la commande</button>
 
   </div>
 
--- page/register.php
+++ page/register.php
@@ -30,37 +30,37 @@
                          <div class="form-group">
                             <label class="col-md-4 control-label">Nom Complet</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input id="fullName" name="fullName" placeholder="Full Name" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input id="fullName" name="fullName" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                          <div class="form-group">
                             <label class="col-md-4 control-label">Addresse Ligne 1</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine1" name="addressLine1" placeholder="Address Line 1" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine1" name="addressLine1" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                          <div class="form-group">
                             <label class="col-md-4 control-label">Addresse Ligne 2</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine2" name="addressLine2" placeholder="Address Line 2" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine2" name="addressLine2" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                          <div class="form-group">
                             <label class="col-md-4 control-label">Pays</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="city" name="city" placeholder="City" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="city" name="city" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                          <div class="form-group">
                             <label class="col-md-4 control-label">Region</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="state" name="state" placeholder="State/Province/Region" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="state" name="state" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                          <div class="form-group">
                             <label class="col-md-4 control-label">Code postal</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="postcode" name="postcode" placeholder="Postal Code/ZIP" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="postcode" name="postcode" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                          <div class="form-group">
@@ -77,13 +77,13 @@
                          <div class="form-group">
                             <label class="col-md-4 control-label">Email</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span><input id="email" name="email" placeholder="Email" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span><input id="email" name="email" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                          <div class="form-group">
                             <label class="col-md-4 control-label">Numéro de téléphone</label>
                             <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span><input id="phoneNumber" name="phoneNumber" placeholder="Phone Number" class="form-control" required="true" value="" type="text"></div>
+                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span><input id="phoneNumber" name="phoneNumber" placeholder="" class="form-control" required="true" value="" type="text"></div>
                             </div>
                          </div>
                       </fieldset>
